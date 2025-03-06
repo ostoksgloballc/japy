5a2099c7db8a0fc1d7f2a3bb2307d943d52ca749
ostoksgloballc <olopeseyu@gmail.com>
2025-03-06 13:05:41 -0500

    Group Project

---
+++ TheWorldAsWeKnowIt.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class TheWorldAsWeKnowIt {
+    public static void main(String[] args) {
+        //This is the list of continents around the world with an assigned numbers
+        LinkedList < String> world = new LinkedList<>();
+        world.add(001, "Africa");
+        world.add(002, "North America");
+        world.add(003, "Asia");
+        world.add(004, "South America");
+        world.add(005, " Europe");
+        world.add(006, "Antarctica");
+        world.add(007, "Oceania");
+    }
+}
